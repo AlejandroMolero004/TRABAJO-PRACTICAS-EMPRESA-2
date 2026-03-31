16504864df644966eee63c67e9e6b1b4e26e601b
AlejandroMolero004 <amoleroo@alumnos.nebrija.es>
2026-03-31 10:58:57 +0200

    almacenar api key en .env

--- src/server.py
+++ src/server.py
@@ -1,5 +1,8 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
 from src.ejecutar_bat import llamar_bat
 from fastapi.templating import Jinja2Templates
@@ -18,6 +21,9 @@ app.mount("/static", StaticFiles(directory="templates/static"), name="styles")
 # esto es para el html templates
 templates = Jinja2Templates(directory="templates")
 
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+
 global resultado
 resultado = None
 
@@ -26,6 +32,11 @@ resultado = llamar_bat()
 url = "http://localhost:8001/"
 
 print(f"El servidor está corriendo en {url}")
+
+@app.exception_handler(RateLimitExceeded)
+def rate_limit_handler(request: Request, exc: RateLimitExceeded):
+    return templates.TemplateResponse("index.html", {"request": request, "resultado": "Has superado el límite de solicitudes. Por favor, inténtalo de nuevo más tarde."}, status_code=429)
+
 @app.get("/")
 def home():
     if resultado:      
@@ -40,8 +51,9 @@ def openIA():
     return {"message": resultado_str}
 
 @app.post("/traducir")
-def traducir(data:codigo_a_traducir):
-    print (data.codigo)
+@limiter.limit("5/minute")
+def traducir(request: Request, data: codigo_a_traducir):
+    print(data.codigo)
     resultado_traduccion = traducir_codigo(data.codigo)
     return {"traduccion": resultado_traduccion}
     
--- src/tratarprompt.py
+++ src/tratarprompt.py
@@ -1,13 +1,17 @@
 from xml.parsers.expat import model
-
+import os 
 from click import prompt
+from dotenv import load_dotenv
 import google.generativeai as genai
-GEMINI_API_KEY="AIzaSyDnU-fiAeQXlcQJm4hRyVaG0Kv4Xc0YV04"
+
+load_dotenv()  # carga el .env
+GOOGLE_API_KEY=  os.getenv("GOOGLE_API_KEY")
 # esta primera funcion la hice paraq que leyera los .cbl que tengo en local
 def tratarprompt():
     global respuesta_modelo
+    genai.configure(api_key=GOOGLE_API_KEY)
     model = genai.GenerativeModel("gemini-1.5-flash")
-    genai.configure(api_key=GEMINI_API_KEY)
+    
 
     with open("C:\\Users\\34684\\Documents\\TRABAJO-PRACTICAS-EMPRESA-2\\hola_mundo.cob", "r", encoding="utf-8") as f:
         cobol_code = f.read()
@@ -28,7 +32,7 @@ def tratarprompt():
 def traducir_codigo(codigo):
     global respuesta_modelo
     model = genai.GenerativeModel("gemini-1.5-flash")
-    genai.configure(api_key=GEMINI_API_KEY)
+    genai.configure(api_key=GOOGLE_API_KEY)
 
     model = genai.GenerativeModel("gemini-3-flash-preview")
 
